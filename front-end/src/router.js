009b84f304540d615ccd688bf04ff462c7968b31
eletrocp <brernolopes@hotmail.com>
2022-09-21 16:25:34 -0300

    feat: implementação de rotas e chamada da pagina login

---
+++ front-end/src/router.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/login';
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Navigate replace to="/login" /> } />
+        <Route path="/login" element={ <Login /> } />
+      </Routes>
+    </BrowserRouter>
+  );
+}
