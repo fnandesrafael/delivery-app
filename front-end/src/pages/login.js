23ed7473f13456442bd10015021a1c3f2190cc9b
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-09-21 18:41:57 -0300

    feat: implementação das verificações dos inputs

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,8 +1,33 @@
-import React, { useState } from 'react';
+// import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const HTTP_NOT_FOUND = 404;
+const MIN_PASSWORD_LENGTH = 6;
 
 function Login() {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [isLoginBtnDisabled, setIsLoginBtnDisabled] = useState(true);
+
+  useEffect(() => {
+    function validateUserPayload() {
+      const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+      if (email.match(emailRegex) && password.length >= MIN_PASSWORD_LENGTH) {
+        return setIsLoginBtnDisabled(false);
+      } return setIsLoginBtnDisabled(true);
+    } validateUserPayload();
+  }, [email, password]);
+
+  const login = async () => {
+    const response = { status: 404 };
+    // const response = await axios.post('localhost:3001/login', { email, password })
+
+    if (response.status === HTTP_NOT_FOUND) {
+      return setErrorMessage('Invalid email or password');
+    } return setErrorMessage(false);
+  };
 
   return (
     <section>
@@ -18,12 +43,26 @@ function Login() {
           data-testid="common_login__input-password"
           onChange={ ({ target }) => { setPassword(target.value); } }
         />
-        <button type="button" data-testid="common_login__button-login">Login</button>
-        <button type="button" data-testid="common_login__button-register">
+        <button
+          type="button"
+          data-testid="common_login__button-login"
+          onClick={ login }
+          disabled={ isLoginBtnDisabled }
+        >
+          Login
+        </button>
+        <button
+          type="button"
+          data-testid="common_login__button-register"
+        >
           Ainda não tenho conta
         </button>
       </div>
-      <p data-test-id="common_login__element-invalid-email" />
+      {errorMessage ? (
+        <p data-test-id="common_login__element-invalid-email">
+          {errorMessage}
+        </p>
+      ) : null}
     </section>
   );
 }
