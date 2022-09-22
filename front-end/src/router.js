07344401c3307150bdf5d68f9f3ef33ece0c16a3
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-09-22 19:01:23 -0300

    feat: implementação de nova rota após login

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/login';
+import Products from './pages/products';
 
 export default function Router() {
   return (
@@ -8,6 +9,7 @@ export default function Router() {
       <Routes>
         <Route path="/" element={ <Navigate replace to="/login" /> } />
         <Route path="/login" element={ <Login /> } />
+        <Route path="/products" element={ <Products /> } />
       </Routes>
     </BrowserRouter>
   );
