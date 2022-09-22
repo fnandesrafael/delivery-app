d4cf5b7db3d229885e3a3e9e565856033d389418
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-09-22 19:01:55 -0300

    refact: refatoração da página login

---
+++ front-end/src/pages/login.jsx
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
+const HTTP_NOT_FOUND = 404;
+const HTTP_OK = 200;
+const MIN_PASSWORD_LENGTH = 6;
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [isLoginBtnDisabled, setIsLoginBtnDisabled] = useState(true);
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    function validateUserPayload() {
+      const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+      if (email.match(emailRegex) && password.length >= MIN_PASSWORD_LENGTH) {
+        return setIsLoginBtnDisabled(false);
+      } return setIsLoginBtnDisabled(true);
+    } validateUserPayload();
+  }, [email, password]);
+
+  const login = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/login', { email, password });
+      console.log(response.status);
+
+      if (response.status === HTTP_OK) {
+        setIsLogged(true);
+        setErrorMessage(false);
+      }
+    } catch (error) {
+      if (error.response.status === HTTP_NOT_FOUND) {
+        return setErrorMessage('email ou senha inválidos');
+      } return setErrorMessage(false);
+    }
+  };
+
+  return (
+    isLogged ? (
+      <Navigate to="/customer/products" />
+    ) : (
+      <section>
+        <h2>Delivery App</h2>
+        <div>
+          <input
+            type="email"
+            data-testid="common_login__input-email"
+            onChange={ ({ target }) => { setEmail(target.value); } }
+          />
+          <input
+            type="password"
+            data-testid="common_login__input-password"
+            onChange={ ({ target }) => { setPassword(target.value); } }
+          />
+          <button
+            type="button"
+            data-testid="common_login__button-login"
+            onClick={ login }
+            disabled={ isLoginBtnDisabled }
+          >
+            Login
+          </button>
+          <button
+            type="button"
+            data-testid="common_login__button-register"
+          >
+            Ainda não tenho conta
+          </button>
+        </div>
+        <p data-testid="common_login__element-invalid-email">
+          {errorMessage}
+        </p>
+      </section>
+    )
+  );
+}
+
+export default Login;
