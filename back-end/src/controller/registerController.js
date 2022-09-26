d5e8979ce416dd91a9bc5dc41cdaf104b8e75211
GabrielBorges <igblborges@gmail.com>
2022-09-26 14:42:58 -0300

    fix: tiramos o trycatch do register  controller

--- back-end/src/controller/registerController.js
+++ back-end/src/controller/registerController.js
@@ -1,13 +1,9 @@
 const { createUser } = require('../service/registerService');
 
 const register = async (req, res) => {
-try {
   const { body } = req;
   const user = await createUser(body);
   res.status(201).json(user);
-} catch (error) {
-  console.log(error);
-}
 };
 
 module.exports = { register };
